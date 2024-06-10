c539d8ff9a83d8ace1152ea93bd2cf048b38ecb5
Michal Tomaszewski <groobytt@gmail.com>
2024-06-10 11:15:31 +0200

    🔨  fix bug with displaying header

--- frontend/zjadlempl/src/app/components/header/header.component.ts
+++ frontend/zjadlempl/src/app/components/header/header.component.ts
@@ -6,18 +6,25 @@ import { AuthService } from 'src/app/services/auth.service';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
+  imports: [RouterModule],
 })
 export class HeaderComponent {
-  constructor(private authService: AuthService) {}
+  constructor(private authService: AuthService, private router: Router) {}
 
   public userLoggedIn = signal(false);
   public username = signal('');
 
+  public loading = signal(false);
+
   ngOnInit(): void {
-    this.authService.checkIfUserIsLoggedIn();
+    this.loading.set(true);
+    this.initSubscriptions();
+    this.loginCheck();
+  }
+
+  initSubscriptions() {
     this.authService.user$.subscribe((user) => {
       if (user) {
         this.userLoggedIn.set(true);
@@ -25,6 +32,25 @@ export class HeaderComponent {
       } else {
         this.userLoggedIn.set(false);
         this.username.set('');
+      }
+    });
+  }
+
+  loginCheck() {
+    this.authService.checkIfCredentailsInStorage().subscribe((res) => {
+      if (res === true) {
+        this.authService.validateToken().subscribe((res2) => {
+          if ((res2 as any).valid === true) {
+            this.userLoggedIn.set(true);
+          } else {
+            this.userLoggedIn.set(false);
+            this.router.navigate(['/login']);
+          }
+          this.loading.set(false);
+        });
+      } else {
+        this.userLoggedIn.set(false);
+        this.loading.set(false);
       }
     });
   }
